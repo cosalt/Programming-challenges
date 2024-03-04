19df12d8ca79cf92640f25350d3f00b1b9ab8838
ArЯay <91860754+cosalt@users.noreply.github.com>
2024-03-04 07:35:39 +0700

    Create code setup.py

---
+++ 2022 - Oct | Nov/4. Average/code setup.py
@@ -0,0 +1,13 @@
+import random
+#The code below is set-up to avoid syntax errors in the flowchart
+SameMonth = []
+for i in range(7):
+  SameMonth.append(random.randint(1, 100))
+
+def GetAverage(UserID):
+  return (random.randint(1, 100))
+
+def Update(UserID, Total):
+  pass
+  
+#Write your program code below
