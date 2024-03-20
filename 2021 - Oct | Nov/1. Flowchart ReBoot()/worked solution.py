15f1c7fe1eee6fdbff1203f84ac4789fa450fb78
Nawin Ray Martin <91860754+cosalt@users.noreply.github.com>
2024-03-20 18:34:37 +0700

    Create worked solution.py

---
+++ 2021 - Oct | Nov/1. Flowchart ReBoot()/worked solution.py
@@ -0,0 +1,14 @@
+#ReBoot is defined to ensure no syntax error
+def ReBoot():
+    pass
+#Write your code below:
+count = 1
+flag = False
+while flag == false and count <= 5:
+  ReBoot()
+  count += 1
+  flag = Check()
+else:
+  if flag == False:
+    Alert(27)
+  
