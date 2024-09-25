29c50b7327f58fe6cf6d1cb0784fe5cc2ffb1737
Nawin Ray Martin <91860754+cosalt@users.noreply.github.com>
2024-09-25 20:39:50 +0700

    Update rock-scissors-paper.py

--- Projects/rock-scissors-paper.py
+++ Projects/rock-scissors-paper.py
@@ -25,3 +25,21 @@ while True:
         while True:
             move = input("Select a move ('r' for rock/'p' for paper/'s' for scissor): ").lower()
             print(f"Computer's Move: {computer_move}")
+            if 'r' in move or 'p' in move or 's' in move():
+                if 'r' in move:
+                    user_move = ROCK
+                elif 'p' in move:
+                    user_move = PAPER
+                elif 's' in move:
+                    user_move = SCISSOR
+                print(f"User Move: {user_move}")
+                output = find_winner(user_move, computer_move)
+                if output == 1:
+                    print("You Won1!!!11!!1!1!!")
+                elif output == -1:
+                    print("Computer Won!!!1!!1!!")
+                else:
+                    print("Tie !!!")
+                break
+            else:
+                print("Invalid input")
