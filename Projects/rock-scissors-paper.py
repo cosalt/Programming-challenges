9db4f307c81bdcf2fabedb9a03aee199efe6041c
Nawin Ray Martin <91860754+cosalt@users.noreply.github.com>
2024-09-25 20:38:05 +0700

    Create rock-scissors-paper.py

---
+++ Projects/rock-scissors-paper.py
@@ -0,0 +1,8 @@
+import random
+
+ROCK = 'rock'
+PAPER = 'paper'
+SCISSOR = 'scissor'
+choices = [ROCK, PAPER, SCISSOR]
+positive = [[PAPER, ROCK], [SCISSOR, PAPER], [ROCK, SCISSOR]]
+negative = [[ROCK, PAPER], [PAPER, SCISSOR], [SCISSOR, ROCK]]
