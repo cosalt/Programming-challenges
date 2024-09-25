e7b036e1278ad3873e3aebc3dc27ba96fcb48b76
Nawin Ray Martin <91860754+cosalt@users.noreply.github.com>
2024-09-25 20:38:21 +0700

    Update rock-scissors-paper.py

--- Projects/rock-scissors-paper.py
+++ Projects/rock-scissors-paper.py
@@ -6,3 +6,14 @@ SCISSOR = 'scissor'
 choices = [ROCK, PAPER, SCISSOR]
 positive = [[PAPER, ROCK], [SCISSOR, PAPER], [ROCK, SCISSOR]]
 negative = [[ROCK, PAPER], [PAPER, SCISSOR], [SCISSOR, ROCK]]
+
+def get_computer_move():
+    move = random.choice(choices)
+    return move
+
+def find_winner(user_move, computer_move):
+    if [user_move, computer_move] in positive:
+        return 1
+    elif [user_move, computer_move] in negative:
+        return -1
+    return 0
